7fc829fc052f108a83d45023ac3ae0656a965da5
Aaryan Pashine <pashineaaryan@gmail.com>
2023-06-16 12:18:32 +0530

    Added percentage closenes to making a circle

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let w = 0
-let h = 0
+let w = 0;
+let h = 0;
+let king = document.querySelector(".king");
+let txt = document.querySelector(".status");
+let close = 0;
 
 document.addEventListener("mousemove", (e) => {
-    console.log(e)
-    w = e.x/2
-    h = e.y/2
-    document.querySelector(".king").style.width = w + "px"
-    document.querySelector(".king").style.height = h + "px"
-    document.querySelector(".king").style.borderRadius = w/10 + "%"
-})
+    console.log(e);
+    w = e.x / 2;
+    h = e.y / 2;
+    bw = e.x / 90;
+    king.style.width = w + "px";
+    king.style.height = h + "px";
+
+    close = Math.abs(1 - e.x / e.y) * -100 + 100;
+    txt.innerText =
+        "You are " + close.toFixed(2) + "% close to making a perfect circle";
+});
